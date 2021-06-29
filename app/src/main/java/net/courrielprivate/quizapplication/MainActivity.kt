e8ca14863995f3ef285c7966998790bea41a2218
pascal <pascal.joret@gmail.com>
2021-06-29 15:56:26 +0200

    Configuration binding

--- app/src/main/java/net/courrielprivate/quizapplication/MainActivity.kt
+++ app/src/main/java/net/courrielprivate/quizapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package net.courrielprivate.quizapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.courrielprivate.quizapplication.databinding.ActivityMainBinding
+
+private lateinit var myBinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        myBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(myBinding.root)
     }
 }
